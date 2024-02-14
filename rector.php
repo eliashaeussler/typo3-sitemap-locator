e31de29dd664f9bedcd6618235729dc836b7a5a1
Elias Häußler <elias@haeussler.dev>
2024-02-14 17:38:22 +0100

    [TASK] Migrate Rector config

--- rector.php
+++ rector.php
@@ -23,10 +23,10 @@ declare(strict_types=1);
 
 use EliasHaeussler\RectorConfig\Config\Config;
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
-use Rector\Symfony\Rector\Class_\CommandDescriptionToPropertyRector;
+use Rector\Symfony\Symfony53\Rector\Class_\CommandDescriptionToPropertyRector;
+use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
     Config::create($rectorConfig, PhpVersion::PHP_81)
